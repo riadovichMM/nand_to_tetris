e53530b8c404e4da28f800f60c1ce99b80d5de24
Magomed Makhmudov <mmriadovich@gmail.com>
2025-09-30 22:36:51 +0300

    init

---
+++ virtual_machine/my/my_asm_codes.py
@@ -0,0 +1,371 @@
+
+#arithmetic and logic command
+ADD_COMMAND = [
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    'A=A-1',
+    'D=D+M',
+    'M=D'
+]
+
+SUB_COMMAND = [
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    'A=A-1',
+    'D=M-D',
+    'M=D',
+]
+
+NEG_COMMAND = [
+    '@SP',
+    'A=M-1',
+    'M=-M',
+]
+
+#stack
+
+
+EQ_COMMAND = [
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    'A=A-1',
+    'D=M-D',
+    '@EQ_:i',
+    'D;JEQ',
+    '@SP',
+    'A=M-1',
+    'M=0',
+    '@END_:i',
+    '0;JMP',
+    '(EQ_:i)',
+    '@SP',
+    'A=M-1',
+    'M=-1',
+    '(END_:i)',
+]
+
+# if (x > y) then true else false
+GT_COMMAND = [
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    'A=A-1',
+    'D=M-D',
+    '@GT_:i',
+    'D;JGT',
+    '@SP',
+    'A=M-1',
+    'M=0',
+    '@END_:i',
+    '0;JMP',
+    '(GT_:i)',
+    '@SP',
+    'A=M-1',
+    'M=-1',
+    '(END_:i)',
+]
+
+# if (x < y) then true else false
+LT_COMMAND = [
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    'A=A-1',
+    'D=M-D',
+    '@LT_:i',
+    'D;JLT',
+    '@SP',
+    'A=M-1',
+    'M=0',
+    '@END_:i',
+    '0;JMP',
+    '(LT_:i)',
+    '@SP',
+    'A=M-1',
+    'M=-1',
+    '(END_:i)',
+]
+
+
+AND_COMMAND = [
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    'A=A-1',
+    'M=D&M',
+]
+
+OR_COMMAND = [
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    'A=A-1',
+    'M=D|M',
+]
+
+
+NOT_COMMAND = [
+    '@SP',
+    'A=M-1',
+    'M=!M',
+]
+
+
+# pop command
+
+POP_LOCAL_COMMAND = [
+    '@LCL',
+    'D=M',
+    '@:n',
+    'D=D+A',
+    '@addr_:i',
+    'M=D',
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    '@addr_:i',
+    'A=M',
+    'M=D'
+]
+
+
+POP_ARGUMENT_COMMAND = [
+    '@ARG',
+    'D=M',
+    '@:n',
+    'D=D+A',
+    '@addr_:i',
+    'M=D',
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    '@addr_:i',
+    'A=M',
+    'M=D'
+]
+
+
+
+POP_THIS_COMMAND = [
+    '@THIS',
+    'D=M',
+    '@:n',
+    'D=D+A',
+    '@addr_:i',
+    'M=D',
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    '@addr_:i',
+    'A=M',
+    'M=D'
+]
+
+
+POP_THAT_COMMAND = [
+    '@THAT',
+    'D=M',
+    '@:n',
+    'D=D+A',
+    '@addr_:i',
+    'M=D',
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    '@addr_:i',
+    'A=M',
+    'M=D'
+]
+
+POP_TEMP_COMMAND = [
+    '@5',
+    'D=A',
+    '@:n',
+    'D=D+A',
+    '@addr_:i',
+    'M=D',
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    '@addr_:i',
+    'A=M',
+    'M=D'
+]
+
+
+POP_STATIC_COMMAND = [
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    '@:n',
+    'M=D'
+]
+
+
+POP_POINTER_COMMAND = [
+    '@3',
+    'D=A',
+    '@:n',
+    'D=D+A',
+    '@addr_:i',
+    'M=D',
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    '@addr_:i',
+    'A=M',
+    'M=D'
+]
+
+
+
+
+# push command
+PUSH_CONSTANT_COMMAND = [
+    '@:n',
+    'D=A',
+    '@SP',
+    'A=M',
+    'M=D',
+    '@SP',
+    'M=M+1',
+]
+
+
+PUSH_LOCAL_COMMAND = [
+    '@LCL',
+    'D=M',
+    '@:n',
+    'D=D+A',
+    'A=D',
+    'D=M',
+    '@SP',
+    'A=M',
+    'M=D',
+    '@SP',
+    'M=M+1',
+]
+
+PUSH_ARGUMENT_COMMAND = [
+    '@ARG',
+    'D=M',
+    '@:n',
+    'D=D+A',
+    'A=D',
+    'D=M',
+    '@SP',
+    'A=M',
+    'M=D',
+    '@SP',
+    'M=M+1',
+]
+
+PUSH_THIS_COMMAND = [
+    '@THIS',
+    'D=M',
+    '@:n',
+    'D=D+A',
+    'A=D',
+    'D=M',
+    '@SP',
+    'A=M',
+    'M=D',
+    '@SP',
+    'M=M+1',
+]
+
+
+PUSH_THAT_COMMAND = [
+    '@THAT',
+    'D=M',
+    '@:n',
+    'D=D+A',
+    'A=D',
+    'D=M',
+    '@SP',
+    'A=M',
+    'M=D',
+    '@SP',
+    'M=M+1',
+]
+
+
+
+PUSH_POINTER_COMMAND = [
+    '@3',
+    'D=A',
+    '@:n',
+    'D=D+A',
+    'A=D',
+    'D=M',
+    '@SP',
+    'A=M',
+    'M=D',
+    '@SP',
+    'M=M+1',
+]
+
+
+PUSH_TEMP_COMMAND = [
+    '@5',
+    'D=A',
+    '@:n',
+    'D=D+A',
+    'A=D',
+    'D=M',
+    '@SP',
+    'A=M',
+    'M=D',
+    '@SP',
+    'M=M+1',
+]
+
+
+PUSH_STATIC_COMMAND = [
+    '@:n',
+    'D=M',
+    '@SP',
+    'A=M',
+    'M=D',
+    '@SP',
+    'M=M+1',
+]
+
+
+
+# program flow
+LABEL_COMMAND = [
+    '(:n)',
+]
+
+GOTO_COMMAND = [
+    '@:n',
+    '0;JMP'
+]
+
+IF_GOTO_COMMAND = [
+    '@SP',
+    'M=M-1',
+    'A=M',
+    'D=M',
+    '@:n',
+    'D;JNE'
+]
+
---
+++ virtual_machine/my/my_vm_translator.py
@@ -0,0 +1,154 @@
+from my_asm_codes import (
+    ADD_COMMAND,
+    EQ_COMMAND,
+    LT_COMMAND,
+    GT_COMMAND,
+    SUB_COMMAND,
+    NEG_COMMAND,
+    AND_COMMAND,
+    OR_COMMAND,
+    NOT_COMMAND,
+    POP_LOCAL_COMMAND,
+    POP_ARGUMENT_COMMAND,
+    POP_THIS_COMMAND,
+    POP_THAT_COMMAND,
+    POP_TEMP_COMMAND,
+    POP_STATIC_COMMAND,
+    POP_POINTER_COMMAND,
+    PUSH_LOCAL_COMMAND,
+    PUSH_POINTER_COMMAND,
+    PUSH_TEMP_COMMAND,
+    PUSH_STATIC_COMMAND,
+    PUSH_ARGUMENT_COMMAND,
+    PUSH_THIS_COMMAND,
+    PUSH_THAT_COMMAND,
+    PUSH_CONSTANT_COMMAND,
+    LABEL_COMMAND,
+    GOTO_COMMAND,
+    IF_GOTO_COMMAND
+)
+
+#vm_code arrays
+vm_code = []
+vm_code_keys = []
+
+asm_code = []
+
+index = 0
+
+def get_vm_code_from_file():
+    f = open("./file_code.vm")
+    for line in f:
+        vm_code.append(line.replace("\n", ""))
+    f.close()
+
+
+def split_by_keys():
+    for line in vm_code:
+        vm_code_keys.append(line.split(" "))
+
+
+def copy_code(asm_commands, value):
+    global index
+    for line in asm_commands:
+        code = line.replace(":n", str(value))
+        code = code.replace(":i", str(index))
+        asm_code.append(code)
+
+
+def create_asm_file():
+    f = open("./asm_code.asm", "+w")
+    for line in asm_code:
+        f.write(line + "\n")
+    f.close()
+
+
+def analysis_and_generate_code():
+    global index
+
+    for vm_command in vm_code_keys:
+        if len(vm_command) == 3:
+            if vm_command[0] == 'push':
+                if vm_command[1] == 'constant':
+                    copy_code(PUSH_CONSTANT_COMMAND, vm_command[2])
+                if vm_command[1] == 'local':
+                    copy_code(PUSH_LOCAL_COMMAND, vm_command[2])
+                if vm_command[1] == 'argument':
+                    copy_code(PUSH_ARGUMENT_COMMAND, vm_command[2])
+                if vm_command[1] == 'this':
+                    copy_code(PUSH_THIS_COMMAND, vm_command[2])
+                if vm_command[1] == 'that':
+                    copy_code(PUSH_THAT_COMMAND, vm_command[2])
+                if vm_command[1] == 'pointer':
+                    copy_code(PUSH_POINTER_COMMAND, vm_command[2])
+                if vm_command[1] == 'temp':
+                    copy_code(PUSH_TEMP_COMMAND, vm_command[2])
+                if vm_command[1] == 'static':
+                    copy_code(PUSH_STATIC_COMMAND, 'BasicTest.' + str(vm_command[2]))
+
+            if vm_command[0] == 'pop':
+                if vm_command[1] == 'local':
+                    copy_code(POP_LOCAL_COMMAND, vm_command[2])
+                if vm_command[1] == 'argument':
+                    copy_code(POP_ARGUMENT_COMMAND, vm_command[2])
+                if vm_command[1] == 'this':
+                    copy_code(POP_THIS_COMMAND, vm_command[2])
+                if vm_command[1] == 'that':
+                    copy_code(POP_THAT_COMMAND, vm_command[2])
+                if vm_command[1] == 'temp':
+                    copy_code(POP_TEMP_COMMAND, vm_command[2])
+                if vm_command[1] == 'static':
+                    copy_code(POP_STATIC_COMMAND, 'BasicTest.' + str(vm_command[2]))
+                if vm_command[1] == 'pointer':
+                    copy_code(POP_POINTER_COMMAND, vm_command[2])
+
+
+        if len(vm_command) == 1:
+            if vm_command[0] == 'add':
+                copy_code(ADD_COMMAND, None)
+            if vm_command[0] == 'eq':
+                copy_code(EQ_COMMAND, None)
+                # index+=1
+            if vm_command[0] == 'lt':
+                copy_code(LT_COMMAND, None)
+                # index+=1
+            if vm_command[0] == 'gt':
+                copy_code(GT_COMMAND, None)
+                # index+=1
+            if vm_command[0] == 'sub':
+                copy_code(SUB_COMMAND, None)
+                # index+=1
+            if vm_command[0] == 'neg':
+                copy_code(NEG_COMMAND, None)
+                # index+=1
+            if vm_command[0] == 'and':
+                copy_code(AND_COMMAND, None)
+                # index+=1
+            if vm_command[0] == 'or':
+                copy_code(OR_COMMAND, None)
+                # index+=1
+            if vm_command[0] == 'not':
+                copy_code(NOT_COMMAND, None)
+                # index+=1
+
+        if len(vm_command) == 2:
+            if vm_command[0] == 'label':
+                copy_code(LABEL_COMMAND, vm_command[1])
+            if vm_command[0] == 'goto':
+                copy_code(GOTO_COMMAND, vm_command[1])
+            if vm_command[0] == 'if-goto':
+                copy_code(IF_GOTO_COMMAND, vm_command[1])
+    index += 1
+
+
+def main():
+    get_vm_code_from_file()
+    split_by_keys()
+    analysis_and_generate_code()
+    create_asm_file()
+
+    print(asm_code)
+
+
+if __name__ == "__main__":
+    main()
