c8dca1b4bd37ca94060d7ec0ff32791b8bd89752
Magomed Makhmudov <mmriadovich@gmail.com>
2025-11-17 23:22:55 +0300

    commit

--- virtual_machine/vm.py
+++ virtual_machine/vm.py
@@ -3,7 +3,7 @@ class VM:
     def __init__(self):
         self.vm_code = []
         self.asm_code = []
-        pass
+        self.global_uuid_command = 0
 
     def open_file_and_get_code(self):
         file = open('./vm_code/code.vm', 'r+')
@@ -14,7 +14,8 @@ class VM:
     def put_to_asm_code(self, code):
         for line in code:
             self.asm_code.append(line)
-    
+
+
     def analys(self):
         for line in self.vm_code:
             line_code_parts = line.split(' ')
@@ -31,12 +32,130 @@ class VM:
         if line_code_part[0] == 'push':
             if line_code_part[1] == 'constant':
                 self.put_to_asm_code([f'@{line_code_part[2]}', 'D=A', '@SP', 'A=M', 'M=D', '@SP', 'M=M+1'])
-            
 
 
     def arithmetic_logic(self, command):
         if command == 'add':
-            self.put_to_asm_code([''])
+            self.put_to_asm_code(['@SP', 'M=M-1', 'A=M', 'D=M', '@SP', 'M=M-1', 'A=M', 'D=D+M', 'M=D', '@SP', 'M=M+1'])
+        if command == 'sub':
+            self.put_to_asm_code(['@SP', 'M=M-1', 'A=M', 'D=M', '@SP', 'M=M-1', 'A=M', 'D=M-D', 'M=D', '@SP', 'M=M+1'])
+        if command == 'neg':
+            self.put_to_asm_code(['@SP', 'M=M-1', 'A=M', 'M=-M', '@SP', 'M=M+1'])
+        if command == 'gt':
+            self.put_to_asm_code([
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=M',
+
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=M-D',
+
+                f'@TRUE_GT_X_{self.global_uuid_command}',
+                'D;JGT',
+
+                '@SP',
+                'A=M',
+                'M=0',
+
+                f'@END_GT_X_{self.global_uuid_command}',
+                '0;JMP',
+
+                f'(TRUE_GT_X_{self.global_uuid_command})',
+                '@SP',
+                'A=M',
+                'M=-1',
+
+                f'(END_GT_X_{self.global_uuid_command})',
+                '@SP',
+                'M=M+1'
+            ])
+        if command == 'lt':
+            # тут возможно ошибка 
+            self.put_to_asm_code([
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=M',
+
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=M-D',
+
+                f'@TRUE_LT_X_{self.global_uuid_command}',
+                'D;JLT',
+
+                '@SP',
+                'A=M',
+                'M=0',
+
+                f'@END_LT_X_{self.global_uuid_command}',
+                '0;JMP',
+
+                f'(TRUE_LT_X_{self.global_uuid_command})',
+                '@SP',
+                'A=M',
+                'M=-1',
+
+                f'(END_LT_X_{self.global_uuid_command})',
+                '@SP',
+                'M=M+1',
+            ])
+        if command == 'and':
+            self.put_to_asm_code([
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=M',
+
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=D&M',
+                'M=D',
+
+                '@SP',
+                'M=M+1',
+            ])
+        if command == 'or':
+            self.put_to_asm_code([
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=M',
+
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=D|M',
+                'M=D',
+
+                '@SP',
+                'M=M+1',
+            ])
+
+        if command == 'not':
+            self.put_to_asm_code([
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'M=!M',
+                '@SP',
+                'M=M+1',
+            ])
+        self.global_uuid_command+=1
+
+
+
+
+    def write_to_asm_file(self):
+        file = open('./asm_code/output.asm', 'w+')
+        for line in self.asm_code:
+            file.write(line + '\n')
+        file.close()
 
 
 
@@ -44,7 +163,8 @@ def main():
     vm = VM()
     vm.open_file_and_get_code()
     vm.analys()
-    print(vm.asm_code)
+    vm.write_to_asm_file()
+    print('finish')
 
 
 
