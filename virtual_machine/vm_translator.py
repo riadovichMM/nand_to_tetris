dbd91a2ed4c4863b400231d58b72ebcd9f056682
Magomed Makhmudov <mmriadovich@gmail.com>
2026-07-20 22:45:39 +0300

    commit

--- virtual_machine/vm_translator.py
+++ virtual_machine/vm_translator.py
@@ -10,7 +10,7 @@ class vm_translator:
         self.vm_code = []
         self.asm_code = ''
         self.id_count = 0
-
+        self.current_function = ''
 
         self.open_file_get_code()
 
@@ -19,12 +19,19 @@ class vm_translator:
     def open_file_get_code(self):
         file = open(os.path.join(self.directory, self.file_name), 'r+')
         for line in file:
+            if line.startswith('//'):
+                continue
+
+            if '//' in line:
+                line = line[:line.index('//')]
+
+            line = line.strip()
+
             line = line.replace('\n', '')
             if not line.startswith('//') and not line == '':
                 self.vm_code.append(line)
         file.close()
 
-    
     def write_asm(self, text):
         self.asm_code = self.asm_code + text + '\n'
 
@@ -41,9 +48,14 @@ class vm_translator:
                 self.push_pop_command(parts)
             if parts[0] in ['add', 'sub', 'and', 'or', 'neg', 'not', 'eq', 'gt', 'lt']:
                 self.arithmetic_logic_command(parts)
+            if parts[0] in ['label', 'goto', 'if-goto']:
+                self.program_flow_command(parts)
+            if parts[0] in ['function', 'call', 'return']:
+                self.function_calling_command(parts)
             self.id_count += 1
 
     def push_pop_command(self, parts):
+        print(parts)
         command, segment, index = parts
 
         if command == 'push':
@@ -269,18 +281,72 @@ class vm_translator:
             self.write_asm('M=M+1')
 
 
+    def program_flow_command(self, parts):
+        command = parts[0]
+        label = parts[1]
+        if command == 'label':
+            if self.current_function:
+                self.write_asm(f'({self.current_function}${label})')
+            else:
+                self.write_asm(f'({label})')
+        if command == 'goto':
+            if self.current_function:
+                self.write_asm(f'@{self.current_function}${label}')
+            else:
+                self.write_asm(f'@{label}')
+
+            self.write_asm('0;JMP')
+        if command == 'if-goto':
+            self.write_asm('@SP')
+            self.write_asm('M=M-1')
+
+            self.write_asm('@SP')
+            self.write_asm('A=M')
+            self.write_asm('D=M')
+
+            if self.current_function:
+                self.write_asm(f'@{self.current_function}${label}')
+            else:
+                self.write_asm(f'@{label}')
+
+            self.write_asm('D;JNE')
+
+    def function_calling_command(self, parts):
+        command = parts[0]
+
+        if command == 'function':
+            function_name = parts[1]
+            local_vars = parts[2]
+            self.write_asm(f'({function_name})')
+            self.write_asm(f'@{local_vars}')
+            self.write_asm('D=A')
+            self.write_asm('@R13')
+            self.write_asm('M=D')
+
+            self.write_asm(f'(LOOP_{self.id_count})')
+            self.write_asm('@R13')
+            self.write_asm('D=M')
+
+            self.write_asm(f'@END_LOOP_{self.id_count}')
+            self.write_asm('D;JEQ')
+
+            self.write_asm('@SP')
+            self.write_asm('A=M')
+            self.write_asm('M=0')
+
+            self.write_asm('@SP')
+            self.write_asm('M=M+1')
 
 
+            self.write_asm('@R13')
+            self.write_asm('M=M-1')
+            self.write_asm(f'@LOOP_{self.id_count}')
+            self.write_asm('0;JMP')
+            
+            self.write_asm(f'(END_LOOP_{self.id_count})')
 
-
-
-
-
-
-
-
-
-
+        if command == 'call':
+            pass
 
 
 
